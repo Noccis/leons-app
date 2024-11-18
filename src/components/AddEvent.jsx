aab35648cfeef5364dbafe55865e66d3909fd155
Noccis <toniwincent83@gmail.com>
2024-11-18 13:04:58 +0100

    started building add event functionality

---
+++ src/components/AddEvent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyledAddEvent } from './styled/AddEvent.styled'
+
+const AddEvent = () => {
+  return (
+    <StyledAddEvent>
+        <button>
+            
+        </button>
+    </StyledAddEvent>
+  )
+}
+
+export default AddEvent
--- src/components/CalendarContainer.jsx
+++ src/components/CalendarContainer.jsx
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 
 const CalendarContainer = () => {
-    const localizer = momentLocalizer(moment);
-    const myEventsList = [
-      {
-        start: moment("2024-11-04T10:00:00").toDate(),
-        end: moment("2024-11-04T11:00:00").toDate(),
-        title: "Test",
-      },
-    ];
+  const localizer = momentLocalizer(moment);
+
+  const myEventsList = [
+    {
+      start: moment("2024-11-04T10:00:00").toDate(),
+      end: moment("2024-11-04T11:00:00").toDate(),
+      title: "Test",
+    },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  function addEvent(){
+
+  };
+
   return (
     <div>
-        <Calendar
+      <Calendar
         localizer={localizer}
         events={myEventsList}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
       />
+      <button onClick={handleModal}>Ny händelse</button>
+      {isOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h2>Här ska det vara en input form</h2>
+            <button onClick={handleModal}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CalendarContainer;
+
+const InputForm = () => {
+  const [event, setEvent] = useState({ title: "", date: null });
+  
+  return (
+    <div>
+
     </div>
   )
 }
-
-export default CalendarContainer
